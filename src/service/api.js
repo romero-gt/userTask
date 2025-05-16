735c94b0c6946f9a5d2ba18be33144f822778786
gt8-sul <gt8sul@gmail.com>
2025-05-15 23:19:56 -0300

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://usertask-d4eb.onrender.com/",
 });
 
 export default instance;
